5b031204a9964d53b354fa4ca5fe537fae09ccd3
beamer <beamer@a49f602f-7962-4aa7-ae61-499af21af415>
2016-04-15 06:31:33 +0000

    Threads fuer As und Bs parallel

--- src/idiom/condwait/OperationsSequential.java
+++ src/idiom/condwait/OperationsSequential.java
@@ -1,12 +1,21 @@
 package idiom.condwait;
 
-import static idiom.condwait.Operation6.a1;
-import static idiom.condwait.Operation6.init;
+import static idiom.condwait.Operation6.*;
 
 public class OperationsSequential {
     public static void main(String... args) {
         init(args);
-
-        a1.exec();
+        Runnable runAs = () -> {
+            a1.exec();
+            a2.exec();
+            a3.exec();
+        };
+        Runnable runBs = () -> {
+            b1.exec();
+            b2.exec();
+            b3.exec();
+        };
+        new Thread(runAs).start();
+        new Thread(runBs).start();
     }
 }
