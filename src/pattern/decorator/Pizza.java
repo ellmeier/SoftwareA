4fc901a44e27ab0b16a21814fda807d6762265f4
beamer <beamer@a49f602f-7962-4aa7-ae61-499af21af415>
2016-04-22 06:52:31 +0000

    Pizza =  Component

---
+++ src/pattern/decorator/Pizza.java
@@ -0,0 +1,11 @@
+package pattern.decorator;
+
+/**
+ * @author R. Schiedermeier, rs@cs.hm.edu
+ * @version 2016-04-22
+ */
+public interface Pizza {
+    int getPrice();
+    boolean isHot();
+    boolean isVegetarian();
+}
